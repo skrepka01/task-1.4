cfb984505f67efa48d5b64582c9d1ba03957b65e
Alexey Demesh <demesaleksej2@gmail.com>
2024-05-29 18:13:51 +0300

    Realise task

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,19 +8,6 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException {
 
-        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
-        //userDaoJDBC.createUsersTable();
-        userDaoJDBC.saveUser(1L,"Alexo","Moore", (byte) 9);
-
-        System.out.println(userDaoJDBC.getAllUsers());
-       // new UserDaoHibernateImpl().createUsersTable();
-       // new UserDaoHibernateImpl().dropUsersTable();
-        //new UserDaoHibernateImpl().saveUser("Alex", "More", (byte) 2);
-        // userDaoHibernate.createUsersTable();
-        // userDaoJDBC.createUsersTable();
-        // userDaoJDBC.saveUser(1,"Бро","Бра", (byte) 19);
-        //userDaoJDBC.createUsersTable();
-        //System.out.println(userDaoJDBC.getAllUsers());
-        // userDaoJDBC.cleanUsersTable();
+        // TODO: 29.05.2024  
     }
 }
